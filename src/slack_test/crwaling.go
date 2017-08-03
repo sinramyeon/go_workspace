890cd46110d14edaad9b0cd3fb959ec8cf9fbe76
hero0926 <esistkalt@naver.com>
2017-08-03 15:47:28 +0900

    트위터 크롤링 하기(API적용), 깃허브 크롤링하기와 문자열용 모듈들

--- src/slack_test/crwaling.go
+++ src/slack_test/crwaling.go
@@ -15,32 +15,17 @@ https://github.com/PuerkitoBio/goquery
 
 ////
 
-// 블로그에서 가져오기
-
-// 트위터에서 가져오기
-/*
-https://twitter.com/devsfarm
-https://twitter.com/WEIRDxMEETUP
-https://twitter.com/golangweekly
-https://twitter.com/newsycombinator
-*/
-
-// 뉴스 사이트에서 가져오기
-
-// 개발자 커뮤니티에서 가져오기
-
-// 깃허브에서 가져오기
-
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"log"
-	"net/http"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/mmcdole/gofeed"
 )
 
 type slackReturn struct {
@@ -48,23 +33,93 @@ type slackReturn struct {
 	URL   string
 }
 
-func getQuote(sym string) string {
-	sym = strings.ToUpper(sym)
-	url := fmt.Sprintf("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=nsl1op&e=.csv", sym)
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Sprintf("error: %v", err)
-	}
-	rows, err := csv.NewReader(resp.Body).ReadAll()
-	if err != nil {
-		return fmt.Sprintf("error: %v", err)
-	}
-	if len(rows) >= 1 && len(rows[0]) == 5 {
-		return fmt.Sprintf("%s (%s) is trading at $%s", rows[0][0], rows[0][1], rows[0][2])
-	}
-	return fmt.Sprintf("unknown response format (symbol was \"%s\")", sym)
+type twitterConf struct {
+	confKey     string
+	confSecret  string
+	tokenKey    string
+	tokenSecret string
 }
 
+// 트위터 읽기
+func TwitterScrape() map[string]string {
+
+	//1. 트위터에 연결하기
+	var env twitterConf
+	env = twitterConf(env)
+	tweetlist := make(map[string]string)
+
+	// https://apps.twitter.com/app/14097310/keys 를 보시면 됩니다.
+
+	config := oauth1.NewConfig(env.confKey, env.confSecret)
+	token := oauth1.NewToken(env.tokenKey, env.confSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	// 테스트 하시려면 유저 확인을 해보시면 좋습니다.
+
+	//verifyParams := &twitter.AccountVerifyParams{
+	//	SkipStatus:   twitter.Bool(true),
+	//	IncludeEmail: twitter.Bool(true),
+	//}
+
+	//로그인 유저 얻기
+	//_, _, _ := client.Accounts.VerifyCredentials(verifyParams)
+
+	// 특정 계정 크롤링 하기
+
+	tweets := GetUserTweets(2, "golangweekly", client)
+
+	for _, v := range tweets {
+		tweetlist[v.Text] = tweetlist[v.Entities.Urls[0].URL]
+	}
+
+	tweets = GetUserTweets(2, "WEIRDxMEETUP", client)
+
+	for _, v := range tweets {
+		tweetlist[v.Text] = tweetlist[v.Entities.Urls[0].URL]
+	}
+
+	tweets = GetUserTweets(2, "devsfarm", client)
+
+	for _, v := range tweets {
+		tweetlist[v.Text] = tweetlist[v.Entities.Urls[0].URL]
+	}
+
+	// 트윗 내용이 키이고 url이 밸류인 걸로 리턴 했습니다(밸류안에 뭘 넣을지 고민중...)
+	return tweetlist
+
+}
+
+// 유저별 트윗을 얻는 모듈
+// 얻어오고 싶은 양, 유저 아이디, 클라이언트 접속정보를 넣어 보세요...
+func GetUserTweets(many int, id string, client *twitter.Client) []twitter.Tweet {
+
+	tweets, _, _ := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
+		Count:           many,
+		ScreenName:      id,
+		IncludeRetweets: twitter.Bool(false),
+	})
+
+	return tweets
+
+}
+
+// rss 블로그 읽기
+func RssScrape() map[string]string {
+
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("http://feeds.twit.tv/twit.xml")
+	fmt.Println(feed.Title)
+
+	// 1. xml 을 struct 에다 env로 넣어두고... 거기서 불러와서
+	// 2. 크롤링 하기 Title 하고 URL이면 될듯
+
+	return nil
+
+}
+
+// OKKY 기술 게시글 읽기
 func OkkyScrape() map[string]string {
 	doc, err := goquery.NewDocument("https://okky.kr/")
 	if err != nil {
@@ -88,4 +143,82 @@ func OkkyScrape() map[string]string {
 
 	return okkylist
 
+}
+
+// 깃허브 고 오픈소스 찾기
+func GoScrape() map[string]string {
+
+	// 깃허브에 연ㅇ결
+
+	doc, err := goquery.NewDocument("https://github.com/trending/go?since=daily")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	githublist := make(map[string]string)
+
+	// EachWithBreak 메서드의 true/false 리턴으로 검색 중 멈추고 싶을 때 멈출 수 있습니다.
+	// 나는 너무많으니까 5개만 가져오는데서 멈춤
+
+	var forLoop int = 0
+
+	doc.Find(".repo-list li").EachWithBreak(func(i int, s *goquery.Selection) bool {
+
+		if forLoop > 4 {
+			return false
+		} else {
+			// Trim 시리즈 적용하는 모듈 나중에 만들어야겠음(너무도 귀찮음)
+			title := s.Find("h3 a").AttrOr("href", "없음")
+			strings.TrimSpace(title)
+			desc := s.Find(".py-1 p").Text()
+			strings.TrimSpace(desc)
+			strings.TrimLeft(desc, " ")
+
+			githublist[title] = desc
+			forLoop++
+			return true
+		}
+	})
+
+	return githublist
+}
+
+// 문자열 슬라이싱용(귀찮음)
+func between(value string, a string, b string) string {
+	// Get substring between two strings.
+	posFirst := strings.Index(value, a)
+	if posFirst == -1 {
+		return ""
+	}
+	posLast := strings.Index(value, b)
+	if posLast == -1 {
+		return ""
+	}
+	posFirstAdjusted := posFirst + len(a)
+	if posFirstAdjusted >= posLast {
+		return ""
+	}
+	return value[posFirstAdjusted:posLast]
+}
+
+func before(value string, a string) string {
+	// Get substring before a string.
+	pos := strings.Index(value, a)
+	if pos == -1 {
+		return ""
+	}
+	return value[0:pos]
+}
+
+func after(value string, a string) string {
+	// Get substring after a string.
+	pos := strings.LastIndex(value, a)
+	if pos == -1 {
+		return ""
+	}
+	adjustedPos := pos + len(a)
+	if adjustedPos >= len(value) {
+		return ""
+	}
+	return value[adjustedPos:len(value)]
 }
